90f9ed5821fce697b931df3218e4425ba370660d
Nicoleta Rosca <nika.roshka@gmail.com>
2020-06-19 21:06:49 +0300

    Created Order object type

---
+++ src/main/java/org/fis/lab/model/Order.java
@@ -0,0 +1,41 @@
+package org.fis.lab.model;
+
+public class Order {
+    private int orderId;
+    private int clientId;
+    private int staffId;
+    private String status;
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public int getStaffId() {
+        return staffId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Order(int orderId, int clientId, int staffId, String status) {
+        this.orderId = orderId;
+        this.clientId = clientId;
+        this.staffId = staffId;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " \"productId\": " + orderId +
+                ", \"Name\": " + "\"" + clientId + "\"" +
+                ", \"quantity\": " + "\"" + staffId + "\"" +
+                ", \"description\": " + "\"" + status + "\""  +
+                '}';
+    }
+}
