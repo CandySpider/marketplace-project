36c07f28c93dffb312ef090499b602adbd299744
Ruben Pirvu <pirvuruben@gmail.com>
2020-06-19 10:21:40 +0300

    FIS-2 #comment Json Functionality Implemented

--- src/main/java/JsonManager.java
+++ src/main/java/JsonManager.java
@@ -1,4 +1,8 @@
 import org.json.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 class Client {
     private int clientId;
     private String firstName;
@@ -50,27 +54,113 @@ class Client {
 
     @Override
     public String toString() {
-        return "Client{" +
-                "clientId=" + clientId +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", adress='" + adress + '\'' +
-                ", email='" + email + '\'' +
-                ", username='" + username + '\'' +
-                ", encryptedPassword='" + encryptedPassword + '\'' +
+        return "{" +
+                " \"clientId\": " +  clientId +
+                ", \"firstName\": " + "\"" + firstName + "\"" +
+                ", \"lastName\": " + "\"" + lastName + "\""  +
+                ", \"phoneNumber\": " + "\"" + phoneNumber + "\"" +
+                ", \"adress\": " + "\"" + adress + "\""  +
+                ", \"email\": " + "\"" + email  + "\"" +
+                ", \"username\": " + "\"" + username  + "\"" +
+                ", \"encryptedPassword\": " + "\"" + encryptedPassword  + "\"" +
                 '}';
     }
 }
-public class JsonManager {
-   public static void main (String [] argv)
-   {
-       // creating JSONObject
-       JSONObject jo = new JSONObject();
 
-       // putting data to JSONObject
-       jo.put("firstName", "John");
-       jo.put("lastName", "Smith");
-       jo.put("age", 25);
-   }
+public class JsonManager {
+    private String filePath;
+    public JsonManager ()
+    {
+        File testFile = new File("");
+        String currentPath = testFile.getAbsolutePath();             //magic spell for finding the path
+        this.filePath  = currentPath+"/src/main/resources/customerStorage.json";
+    }
+    public void init ()
+    {
+        try {
+            JSONObject unu = new JSONObject();
+            unu.put("personalData",new JSONArray());
+            PrintWriter writer = new PrintWriter(this.filePath);
+            writer.print(unu.toString());
+            writer.close();
+            System.out.println(unu);
+        }
+        catch (IOException e )
+        {
+            e.printStackTrace();
+        }
+    }
+    public void addJsonObj (Client theOne)
+    { try {
+        String contents = new String((Files.readAllBytes(Paths.get(this.filePath))));
+        JSONObject myObject = new JSONObject(contents);
+        JSONArray myArray = myObject.getJSONArray("personalData");
+        myArray.put(new JSONObject(theOne.toString()));
+        System.out.println(myObject);
+        PrintWriter writer = new PrintWriter(this.filePath);
+        writer.print(myObject);
+        writer.close();
+    }
+    catch (IOException e )
+    {
+        e.printStackTrace();
+    }
+    }
+
+    public void removeJsonObj (int i )
+    {
+        try {
+            String contents = new String((Files.readAllBytes(Paths.get(this.filePath))));
+            JSONObject myObject = new JSONObject(contents);
+            JSONArray myArray = myObject.getJSONArray("personalData");
+            myArray.remove(i);
+            System.out.println(myObject);
+            PrintWriter writer = new PrintWriter(this.filePath);
+            writer.print(myObject);
+            writer.close();
+        }
+        catch (IOException e )
+        {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] argv) {
+//Creating a JSONObject object
+
+        //Inserting key-value pairs into the json object
+
+//        File testFile = new File("");
+//        String currentPath = testFile.getAbsolutePath();             //magic spell for finding the path
+//        String first = currentPath+"/src/main/resources/customerStorage.json";
+
+
+
+            Client experimentalClient = new Client(1,"Ion","Castan","0756444890","Jud. PH,Oras. Bacanesti","youexp@gmail.com","xxDemonSlayerxx","gigel");
+            JSONObject iExp = new JSONObject(experimentalClient);
+
+            JsonManager manageStuff = new JsonManager();
+            manageStuff.init();
+            manageStuff.addJsonObj(experimentalClient);
+            manageStuff.addJsonObj(experimentalClient);
+            manageStuff.removeJsonObj(1);
+            //manageStuff.addJsonObj(experimentalClient);
+           // manageStuff.addJsonObj(experimentalClient);
+
+            // JSONArray emails = o.getJSONArray("emails");
+            // for (int i = 0; i < emails.length(); i++) {
+            //    System.out.println(emails.get(i));
+            // }
+            // JSONObject family = o.getJSONObject("family");
+            // System.out.println(family.getString("spouse"));
+//            JSONArray stocare = o.getJSONArray("personalData");
+//            System.out.println(stocare.get(0));
+//            System.out.println(stocare.get(1));
+//            System.out.println(experimentalClient.toString());
+//            System.out.println(iExp);
+//            JSONObject what = new JSONObject(experimentalClient.toString());
+//            System.out.println(what);
+
+
+
+    }
 }
