5c0735a246bb5d1af38ade262f0350c6645edd95
Nicoleta Rosca <nika.roshka@gmail.com>
2020-07-01 23:30:08 +0300

    FIS-32 #comment Updated main for initializing the app

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,30 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import java.io.IOException;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
 
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("UI.fxml"));
+        primaryStage.setTitle("lemon");
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.show();
+
+
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
